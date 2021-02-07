59951890de174ae6b107271f7aa25abeec4d7061
Lucius VanSlyke <lu.vanslyke@gmail.com>
2021-02-07 13:27:34 -0500

    commit tree a9db6b6645373a1dd12d5268dfbc342b804aab4f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email){
+    let formData = {
+        name, 
+        email
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+    return fetch("http://localhost:3000/users", configObj)
+    .then((response) => response.json())
+    .then((data) => { 
+        document.body.innerHTML = object["id"]
+        console.log(data) 
+    })
+    .catch((error) => {
+        alert("Error"); 
+        console.log(error.message); 
+    })
+}
